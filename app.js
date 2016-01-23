2d35397e18adcf6ad0b44e2d162f2ec7984a5a99
zbay <zlwilliams1@gmail.com>
2016-01-23 00:00:59 +0000

    Heroku URL

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var routes = require('./routes');
 var session = require('client-sessions');
 //var dotenv = require('dotenv').load();
 
-//mongoose.connect('mongodb://heroku_l51d2vps:2aq0iso1kf2gjkv2b8tb1nm5g8@ds045475.mongolab.com:45475/heroku_l51d2vps', function (err, db)
-mongoose.connect('mongodb://localhost:27017/votingapp', function (err, db)
+mongoose.connect('mongodb://heroku_l51d2vps:2aq0iso1kf2gjkv2b8tb1nm5g8@ds045475.mongolab.com:45475/heroku_l51d2vps', function (err, db)
+//mongoose.connect('mongodb://localhost:27017/votingapp', function (err, db)
 {
  if (err) {
       throw new Error('Database failed to connect!');
